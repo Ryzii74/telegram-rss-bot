db30af77c8de0b94edae84c19df631f6c59ab791
ryzii <ryzii@ryzii-macbook.local>
2017-09-29 00:33:15 +0300

    + show all urls with http on command get

--- commands/getAllRss.js
+++ commands/getAllRss.js
@@ -5,7 +5,7 @@ const users = require('../libs/users');
 module.exports = async function (msg) {
     const userId = msg.from.id;
     const links = await users.getAllLinks(userId);
-    const result = links.map((el, index) => `${index + 1}) ${el}`).join('\n');
+    const result = links.map((el, index) => `${index + 1}) http://${el}`).join('\n');
     if (result) {
         return 'Ваш список:\n' + result;
     } else {
