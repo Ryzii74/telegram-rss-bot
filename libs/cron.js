e3117fa82187331eab489acab607c88c47eb3607
ryzii <ryzii@ryzii-macbook.local>
2017-10-01 18:05:11 +0300

    + add date of updating rss

--- libs/cron.js
+++ libs/cron.js
@@ -13,6 +13,7 @@ module.exports = {
     },
 
     async loadAll() {
+        console.log('Обновление данных', new Date());
         const items = await rss.getAll();
         for (let i = 0, max = items.length; i < max; i++) {
             const item = items[i];
@@ -25,12 +26,12 @@ module.exports = {
                 continue;
             }
 
-            const newArticles = Articles.getNew(item.articles, articles);
+            const newArticles = Articles.getNew(item.articles, articles.map(el => el.link));
             console.log(item.url, `новых новостей - ${newArticles.length}`);
             if (newArticles.length === 0) continue;
 
             const usersToSend = await users.getLinkSubscribers(item.url);
-            newArticles.reverse().forEach(article => bot.sendToUsers(usersToSend, article.link));
+            newArticles.reverse().forEach(article => bot.sendToUsers(usersToSend, article));
 
             await rss.update(item.url, item.articles || [], newArticles || [], 0);
         }
